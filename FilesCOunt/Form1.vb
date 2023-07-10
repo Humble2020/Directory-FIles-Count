e8ece59e00ff0258a009be6df6384a26c49feeb9
JOSEPH ALAKPA <alakpauzezi2017@gmail.com>
2023-07-10 14:27:43 +0100

    updated the textbox to point to the users folder upon load of the program.

---
+++ FilesCOunt/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Textbox1.text = "C:\Users\"
+    End Sub
+  
+    Public Function FileCount(ByVal DirectoryNAme As String) As Long
+        Dim objFS As New Scripting.FileSystemObject
+        Dim objFolder As Scripting.Folder
+        If objFS.FolderExists(DirectoryNAme) Then
+            objFolder = objFS.GetFolder(DirectoryNAme)
+            FileCount = objFolder.Files.Count
+        End If
+   End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = FileCount(Me.TextBox1.Text).ToString
+    End Sub
+End Class
